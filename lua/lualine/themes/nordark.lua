f5f142a7c9f3bd8ee304db02239ab28dc261829c
Tim Schumacher <tim.schumacher@panther.com>
2023-04-25 18:29:53 -0400

    Luatab: Z section constant color

--- lua/lualine/themes/nordark.lua
+++ lua/lualine/themes/nordark.lua
@@ -7,22 +7,28 @@ nordark.normal = {
     b = { fg = c.grey_fg2, bg = c.polar_night.black2 },
     c = { fg = c.snow_storm.origin, bg = c.statusline_bg },
     x = { fg = c.white, bg = c.statusline_bg },
+    -- set to a constant color for working dir
+    z = { fg = c.polar_night.origin, bg = c.folder_bg },
 }
 
 nordark.insert = {
     a = { fg = c.polar_night.origin, bg = c.dark_purple, bold = true },
+    z = { fg = c.polar_night.origin, bg = c.folder_bg },
 }
 
 nordark.visual = {
     a = { fg = c.polar_night.origin, bg = c.frost.artic_ocean, bold = true },
+    z = { fg = c.polar_night.origin, bg = c.folder_bg },
 }
 
 nordark.replace = {
     a = { fg = c.polar_night.origin, bg = c.aurora.orange, bold = true },
+    z = { fg = c.polar_night.origin, bg = c.folder_bg },
 }
 
 nordark.command = {
     a = { fg = c.polar_night.origin, bg = c.aurora.green, bold = true },
+    z = { fg = c.polar_night.origin, bg = c.folder_bg },
 }
 
 nordark.inactive = {
