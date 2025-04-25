0ce7b01f9a93922a4337dee981bc2fc0ad761f1d
Tim Schumacher <tim.schumacher@panther.com>
2025-04-25 10:28:48 -0400

    Lualine cleanups

--- lua/lualine/themes/nordark.lua
+++ lua/lualine/themes/nordark.lua
@@ -1,40 +1,52 @@
 local c = require("nord.colors").palette
 
-local nordark = {}
+local M = {}
 
-nordark.normal = {
-    a = { fg = c.polar_night.origin, bg = c.frost.artic_water, bold = true },
-    b = { fg = c.grey_fg2, bg = c.polar_night.black2 },
-    c = { fg = c.snow_storm.origin, bg = c.statusline_bg },
-    x = { fg = c.white, bg = c.statusline_bg },
-    -- set to a constant color for working dir
-    z = { fg = c.polar_night.origin, bg = c.folder_bg },
+M.normal = {
+  a = { fg = c.polar_night.origin, bg = c.frost.artic_water, bold = true },
+  b = { fg = c.grey_fg2, bg = c.polar_night.black2 },
+  c = { fg = c.snow_storm.origin, bg = c.statusline_bg },
+
+  x = { fg = c.white, bg = c.statusline_bg },
+  y = { fg = c.grey_fg2, bg = c.polar_night.black2 },
+  z = { fg = c.polar_night.origin, bg = c.frost.artic_water },
+
+  -- set to a constant color for working dir
+  --z = { fg = c.polar_night.origin, bg = c.folder_bg },
 }
 
-nordark.insert = {
-    a = { fg = c.polar_night.origin, bg = c.dark_purple, bold = true },
-    z = { fg = c.polar_night.origin, bg = c.folder_bg },
+M.insert = {
+  a = { fg = c.polar_night.origin, bg = c.aurora.purple_dim, bold = true },
+  --b = { fg = c.aurora.purple_dim, bg = c.polar_night.black2 },
+  --
+  --z = { fg = c.polar_night.origin, bg = c.aurora.purple_dim },
+  z = { fg = c.polar_night.origin, bg = c.aurora.purple_dim },
 }
 
-nordark.visual = {
-    a = { fg = c.polar_night.origin, bg = c.frost.artic_ocean, bold = true },
-    z = { fg = c.polar_night.origin, bg = c.folder_bg },
+M.visual = {
+  a = { fg = c.polar_night.origin, bg = c.frost.polar_water, bold = true },
+  z = { fg = c.polar_night.origin, bg = c.frost.polar_water },
 }
 
-nordark.replace = {
-    a = { fg = c.polar_night.origin, bg = c.aurora.orange, bold = true },
-    z = { fg = c.polar_night.origin, bg = c.folder_bg },
+M.replace = {
+  a = { fg = c.polar_night.origin, bg = c.aurora.orange, bold = true },
+  z = { fg = c.polar_night.origin, bg = c.aurora.orange },
 }
 
-nordark.command = {
-    a = { fg = c.polar_night.origin, bg = c.aurora.green, bold = true },
-    z = { fg = c.polar_night.origin, bg = c.folder_bg },
+M.command = {
+  a = { fg = c.polar_night.origin, bg = c.frost.ice, bold = true },
+  z = { fg = c.polar_night.origin, bg = c.frost.ice },
 }
 
-nordark.inactive = {
-    a = { fg = c.white, bg = c.polar_night.black2 },
-    b = { fg = c.white, bg = c.polar_night.black2 },
-    c = { fg = c.white, bg = c.polar_night.black2 },
+M.terminal = {
+  a = { fg = c.polar_night.origin, bg = c.aurora.yellow },
+  z = { fg = c.polar_night.origin, bg = c.aurora.yellow },
 }
 
-return nordark
+M.inactive = {
+  a = { fg = c.white, bg = c.polar_night.black2 },
+  b = { fg = c.white, bg = c.polar_night.black2 },
+  c = { fg = c.white, bg = c.polar_night.black2 },
+}
+
+return M
